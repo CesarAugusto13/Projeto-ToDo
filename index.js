07be686a5a1a70f899e2b1efa29e270f55733a20
César <cesiha13@gmail.com>
2023-12-02 03:52:43 -0300

    Salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ app.set("view engine" , "handlebars")
 
 app.use(express.static('public'))
 
-app.get('/', (req,res) => {
-    res.render("home")
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/criar', (requisicao,resposta) =>{
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `INSERT INTO tarefas(descricao, completa )
+        VALUES ('${descricao}', '${completa}')
+    `
+    conexao.query(sql, (erro) =>{
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+
+})
+
+app.get('/', (requisicao,resposta) => {
+    resposta.render("home")
 })
 
 const conexao = mysql.createConnection({
